a3b34c865aa6e2067d7e5b2eda0b198349d836bb
Panela Leung <leungpanela@gmail.com>
2019-09-13 02:48:06 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,8 +8,8 @@ class MP3Importer
     @files = Dir.entries(path).grep(/.*\.mp3/)
   end
 
-  def import(files)
-    files.each{ |file| Song.new_by_filename(file) }
+  def import
+    @files.each{ |file| Song.new_by_filename(file) }
   end
 
 end
